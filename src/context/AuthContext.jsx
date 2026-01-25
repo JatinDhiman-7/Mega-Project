eeb3a90288f796a67652ed276a69c568966fc70f
unknown <jatin.careeer@gmail.com>
2026-01-25 08:30:04 +0530

    update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,15 +1,12 @@
 // src/context/AuthContext.js
-import { useEffect, createContext, useState, useMemo } from "react";
-import { toast } from "react-toastify";
+import {createContext, useState, useMemo } from "react";
 import { logoutUser } from "../api/auth";
-import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(
     Boolean(localStorage.getItem("access"))
   );
   const login = (access) => {
-    // console.log(access)
     localStorage.setItem("access", access);
     setIsLoggedIn(true);
   };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { profiledata } from "../api/auth";
 import { useEffect, useState } from "react";
+import ProfileImage from '../assets/images/profileAvtar.jpg'
 const Profile = () => {
     const [profiles, setProfiles] = useState([])
     useEffect(() => {
@@ -11,21 +12,26 @@ const Profile = () => {
     }, []);
 
     return (
-        <div>
-            <h2>User Profiles</h2>
-            {profiles.length === 0 ? (
-                <p>No profiles found.</p>
-            ) : (
-                <ul>
-                    {profiles.map((profile, index) => (
-                        <li key={index}>
-                            <strong>Name:</strong> {profile.first_name} <br />
-                            <strong>Phone:</strong> {profile.phone} <br />
-                            <strong>Address:</strong> {profile.address}
-                        </li>
-                    ))}
-                </ul>
-            )}
+        <div className="profile">
+            <div>
+                {profiles.length === 0 ? (
+                    <div>
+                        <img src={ProfileImage} alt="profileimg" className="profileImg" />
+                        <p>No profiles found.</p>
+                    </div>
+                ) : (
+                    <ul>
+                        <img src={ProfileImage} alt="profileimg" className="profileImg" />
+                        {profiles.map((profile, index) => (
+                            <li key={index}>
+                                <strong>Name:</strong> {profile.first_name} <br />
+                                <strong>Phone:</strong> {profile.phone} <br />
+                                <strong>Address:</strong> {profile.address}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </div>
     );
 
